fd712dacd332bfac413665a597a3fe97eebe4966
patemob <paytonmobley@gmail.com>
2023-02-03 04:09:20 -0500

    Add project files.

---
+++ WeeklyAssignment2/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmBurgers
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+    Private Sub lblInstructions_Click(sender As Object, e As EventArgs) Handles lblInstructions.Click
+
+    End Sub
+End Class
